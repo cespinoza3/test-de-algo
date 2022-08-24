2eae053434cb4022ca3173992f696b1263cdc330
Cespinoza <103214138+cespinoza3@users.noreply.github.com>
2022-08-24 20:01:47 +0000

    Agregar mensaje adios y comentario

--- Main.java
+++ Main.java
@@ -1,5 +1,10 @@
 class Main {
+  /**
+  * Dice hola y adios
+  * @returns void
+  */
   public static void main(String[] args) {
     System.out.println("Hola !");
+    System.out.println("Adios");
   }
 }
